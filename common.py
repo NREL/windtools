b14940722eed0cda85704ec7e575a476167d105b
Eliot Quon <eliot.quon@nrel.gov>
2020-01-23 15:24:02 -0700

    Add common.fit_powerlaw() function
    Based on fit_power_law_alpha() from NWTC.abl.meteorology
    
    Inputs may be arrays z and U; pd.Series or pd.DataFrame with height as
    the index

--- common.py
+++ common.py
@@ -74,5 +74,60 @@ def calc_uv(df=None,wspd='wspd',wdir='wdir'):
     v = wspd * np.sin(ang)
     return u,v
 
+def fit_powerlaw(df=None,z=None,U=None,zref=80.0,Uref=None):
+    """Calculate power-law exponent to estimate shear.
+
+    Parameters
+    ==========
+    df : pd.DataFrame, optional
+        Calculate from data columns; index should be height values
+    U : str or array-like, optional
+        An array of wind speeds if dataframe 'df' is not provided speeds
+    z : array-like, optional
+        An array of heights if dataframe 'df' is not provided
+    zref : float
+        Power-law reference height
+    Uref : float, optional
+        Power-law reference wind speed; if not specified, then the wind
+        speeds are evaluatecd at zref to get Uref
+
+    Returns
+    =======
+    alpha : float or pd.Series
+        Shear exponents
+    R2 : float or pd.Series
+        Coefficients of determination
+    """
+    from scipy.optimize import curve_fit
+    # generalize all inputs
+    if df is None:
+        assert (U is not None) and (z is not None)
+        df = pd.DataFrame(U, index=z)
+    elif isinstance(df,pd.Series):
+        df = pd.DataFrame(df)
+    # make sure we're only working with above-ground values
+    df = df.loc[df.index > 0]
+    z = df.index
+    logz = np.log(z) - np.log(zref)
+    # evaluate Uref at zref, if needed
+    if Uref is None:
+        Uref = df.loc[zref]
+    elif not hasattr(Uref, '__iter__'):
+        Uref = pd.Series(Uref,index=df.columns)
+    # calculate shear coefficient
+    alpha = pd.Series(index=df.columns)
+    R2 = pd.Series(index=df.columns)
+    def fun(x,*popt):
+        return popt[0]*x
+    for col,U in df.iteritems():
+        logU = np.log(U) - np.log(Uref[col])
+        popt, pcov = curve_fit(fun,xdata=logz,ydata=logU,p0=0.14,bounds=(0,1))
+        alpha[col] = popt[0]
+        U = df[col]
+        resid = U - Uref[col]*(z/zref)**alpha[col]
+        SSres = np.sum(resid**2)
+        SStot = np.sum((U - np.mean(U))**2)
+        R2[col] = 1.0 - (SSres/SStot)
+    return alpha.squeeze(), R2.squeeze()
 
 
