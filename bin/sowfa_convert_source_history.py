5d36b23591d191868ab0c31bf5460436700e6f9f
Eliot Quon <eliot.quon@nrel.gov>
2020-09-09 17:26:00 -0600

    Add docstring and update licenses in header

--- bin/sowfa_convert_source_history.py
+++ bin/sowfa_convert_source_history.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
+
+# Copyright 2020 NREL
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy of the
+# License at http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+"""
+Convert precursor sourceHistory into OpenFOAM dictionaries to include in
+constant/ABLProperties as _given_ momentum and/or temperature source
+terms.
+
+USAGE: sowfa_convert_source_history.py [/path/to/postProcessing/sourceHistory]
+
+where the default path is ./postProcessing/sourceHistory.
+
+Upon completion, the script will write out:
+- constant/givenSourceU
+- constant/givenSourceT
+
+"""
 import sys, os
 import numpy as np
 from windtools.SOWFA6.postProcessing.sourceHistory import SourceHistory
