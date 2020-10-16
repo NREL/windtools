21ab52634d27f3c4a87f48b8c02af3b3c706e5bd
Regis Thedin <registhedin@gmail.com>
2020-10-15 19:29:26 -0600

    Move restart time print to processdirs
    Moved a print statement from __init__ to _processdirs. This was done for
    better support for different child classes, where the super().__init__
    is used, but not super()._processdirs.

--- windtools/SOWFA6/postProcessing/reader.py
+++ windtools/SOWFA6/postProcessing/reader.py
@@ -73,8 +73,6 @@ class Reader(object):
         self.simTimeDirs = [ x[1] for x in sorted(zip(self.simStartTimes,self.simTimeDirs)) ]
         self.simStartTimes.sort()
 
-        print('Simulation (re)start times:',self.simStartTimes)
-
         # process all output dirs
         if len(self.simTimeDirs) > 0:
             self._processdirs( self.simTimeDirs, **kwargs )
@@ -94,6 +92,8 @@ class Reader(object):
         is updated, e.g.:
             ${timeDir}/U is appended to the array self.U
         """
+        print('Simulation (re)start times:',self.simStartTimes)
+
         if isinstance( varList, (str,) ):
             if varList.lower()=='all':
                 # special case: read all vars
