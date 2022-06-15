293c6002048d460a19da9189de1c36fc9369ea0c
Eliot Quon <eliot.quon@nrel.gov>
2022-06-15 10:41:37 -0600

    Update datetime processing

--- windtools/amrwind/post_processing.py
+++ windtools/amrwind/post_processing.py
@@ -3,22 +3,25 @@ import xarray as xr
 
 class ABLStatistics(object):
 
-    def __init__(self,fpath,startdatetime=None):
+    def __init__(self,fpath,startdate=None):
         self.fpath = fpath
-        if startdatetime:
-            self.datetime0 = pd.to_datetime(startdatetime)
+        if startdate:
+            self.datetime0 = pd.to_datetime(startdate)
         else:
             self.datetime0 = None
         self._load_timeseries()
         #self._load_timeheightseries()
 
-    def _load_timeseries(self):
-        ds = xr.open_dataset(self.fpath)
+    def _setup_time_coords(self,ds):
         if self.datetime0:
             dt = self.datetime0 + pd.to_timedelta(ds['time'], unit='s')
-            ds = ds.assign_coords({'num_time_steps':dt})
-            print(ds)
+            ds = ds.assign_coords({'datetime':('num_time_steps',dt)})
             ds = ds.swap_dims({'num_time_steps':'datetime'})
         else:
             ds = ds.swap_dims({'num_time_steps':'time'})
+        return ds
+
+    def _load_timeseries(self):
+        ds = xr.open_dataset(self.fpath)
+        ds = self._setup_time_coords(ds)
         self.ds = ds
