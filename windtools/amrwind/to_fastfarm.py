5e90b0a73348bcba3888f8ece1b6e5ed3e6c1bb9
AbhineetGupta <abhineet.gupta@nrel.gov>
2023-07-07 16:49:58 -0600

    Add MPI support for writing VTK files in the example to_fastfarm.py

--- windtools/amrwind/to_fastfarm.py
+++ windtools/amrwind/to_fastfarm.py
@@ -1,14 +1,16 @@
 import os
 import numpy as np
-import windtools.amrwind.post_processing as pp
 import multiprocessing as mp
+from mpi4py import MPI
+
+import windtools.amrwind.post_processing as pp
 
 
-def to_ff(file_path, output_path, group_name, i_start, i_end, numcores, vtkstartind=0):
+def dristribute_nodes(
+    file_path, output_path, group_name, i_start, i_end, vtkstartind=0
+):
     """
-    An example function to write VTK files parallelly from AMR-Wind simulation.
-    This code spawns a separate process to write each VTK file.
-    The total number of parallel processes can be selected using the input `numcores'.
+    An example function to write VTK files parallelly from AMR-Wind simulation using MPI
 
     Args:
     -----
@@ -22,9 +24,6 @@ def to_ff(file_path, output_path, group_name, i_start, i_end, numcores, vtkstart
         Initial index of the VTK file to be written (This is usually )
     i_end: int
         Final index of the VTK file to be written
-    numcores: int
-        Number of parallel processes spawned at a time. This should be set to
-        the number of cores in the CPU,
     vtkstartind: int, optional, default=0
         Index by which the names of the vtk files will be shifted.
         This is useful for saving files starting from a non-zero time-step when AMR-Wind crashes
@@ -33,10 +32,10 @@ def to_ff(file_path, output_path, group_name, i_start, i_end, numcores, vtkstart
     Example:
     -------
     The function can be used as follows:
-    
+
     >>> import windtools.amrwind.to_fastfarm as tf
     >>> import time
-    >>> 
+    >>>
     >>> file_path = '../sampling_lr96000.nc'
     >>> output_path = 'output'
     >>> group_name = 'Low'
@@ -44,36 +43,62 @@ def to_ff(file_path, output_path, group_name, i_start, i_end, numcores, vtkstart
     >>> shiftind=0
     >>> i_start = 0
     >>> i_end = 7500
-    >>> 
+    >>>
     >>> startTime = time.time()
-    >>> tf.to_ff(file_path,output_path,group_name,i_start,i_end,numcores,shiftind)
+    >>> tf.distribute_nodes(file_path,output_path,group_name,i_start,i_end,shiftind)
     >>> executionTime = (time.time() - startTime)
     >>> print('Execution time in seconds: ' + str(executionTime))
 
     """
 
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    # Calculate the workload for each process
+    workpernode = (i_end - i_start + 1) // size
+    remainder = (i_end - i_start + 1) % size
+
+    # Determine the start and end indices for each process
+    start_index = i_start + rank * workpernode + min(rank, remainder)
+    end_index = start_index + workpernode + (rank < remainder)
+    print(f"The current rank is {rank}")
+    disritube_cores(
+        file_path, output_path, group_name, start_index, end_index, vtkstartind=0
+    )
+
+
+def disritube_cores(file_path, output_path, group_name, i_start, i_end, vtkstartind=0):
+
+    # Find number of cores
+    numcores = mp.cpu_count()
+    # numcores = 2
+
     # Read sample and prints the groups contained in it
     sample = pp.Sampling(file_path)
     print(sample)
     group_path = os.path.join(output_path, group_name)
+    # dst1 = sample.read_single_group('Low')
 
     # numbatches is the number of batches of `numcores` processes that will be spawned
     numbatches = (i_end - i_start + 1) // numcores + 1
 
     for batchi in np.arange(numbatches):
-        Proc_all = []   # List to stores all the processes for a batch.
+        Proc_all = []  # List to stores all the processes for a batch.
         for corei in np.arange(numcores):
-            ind = batchi * numcores + corei + i_start   # Index of the VTK file to be written
-            if ind > i_end:                             # Don't spawn more processes if last index is reached
+            ind = (
+                batchi * numcores + corei + i_start
+            )  # Index of the VTK file to be written
+            if ind > i_end:  # Don't spawn more processes if last index is reached
                 break
-            
+
             # Spawn one process for each index
             p = mp.Process(
                 target=parfunc,
                 args=(sample, group_name, group_path, ind, ind + 1, vtkstartind),
             )
 
-            # Start the process 
+            # Start the process
             p.start()
 
             # Store all the processes spawned in a batch in a list
@@ -93,4 +118,5 @@ def parfunc(sample, dataset, group_path, ind_i, ind_f, vtkstartind):
         itime_i=ind_i,
         itime_f=ind_f,
         vtkstartind=vtkstartind,
+        verbose=False,
     )
